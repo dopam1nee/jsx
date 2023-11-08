9fdcd8d0dfb9de5a00b9ec6405e5a951b24e6987
dopa <ISPQ@bk.ru>
2023-11-08 17:19:26 +0200

    with JSX

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg'
+import './App.css'
+import { createElement } from 'react'
+
+export const App = () => {
+	const currentYear = new Date().getFullYear() // декларативный
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>{currentYear}</p>
+			</header>
+		</div>
+	)
+}
